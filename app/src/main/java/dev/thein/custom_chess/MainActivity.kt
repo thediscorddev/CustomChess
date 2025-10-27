5de70f0d72630e9215c2e1de31b0d6339c634fe5
thediscorddev <paypalchplay@gmail.com>
2025-10-27 15:53:42 +0700

    Forgor

--- app/src/main/java/dev/thein/custom_chess/MainActivity.kt
+++ app/src/main/java/dev/thein/custom_chess/MainActivity.kt
@@ -8,13 +8,56 @@ import android.widget.EditText
 import dev.thein.custom_chess.R
 
 class MainActivity : AppCompatActivity() {
+    fun InitMenuButton()
+    {
+       val homeButton = findViewById<Button>(R.id.homeButton) 
+       val AllGamesButton = findViewById<Button>(R.id.AllGamesButton) 
+       val settingsButton = findViewById<Button>(R.id.settingsButton) 
+       val AboutButton = findViewById<Button>(R.id.AboutButton) 
+       homeButton.setOnClickListener {
+        InitMainView()
+       }
+       AllGamesButton.setOnClickListener {
+        SwitchToAllGames()
+       }
+       settingsButton.setOnClickListener {
+        SwitchToSettings()
+       }
+       AboutButton.setOnClickListener {
+        SwitchToAbout()
+       }
+    }
+    fun SwitchToSettings()
+    {
+        setContentView(R.layout.activity_settings)
+        // listen to button
+        InitMenuButton()
+    }
+    fun SwitchToAllGames()
+    {
+        setContentView(R.layout.activity_allgames)
+        // listen to button
+        InitMenuButton()
+    }
+    fun SwitchToAbout()
+    {
+        setContentView(R.layout.activity_about)
+        // listen to button
+        InitMenuButton()
+    }
+    fun InitMainView()
+    {
+        setContentView(R.layout.activity_main)
+        // listen to button
+        InitMenuButton()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val prefs = getSharedPreferences("user_data", MODE_PRIVATE)
         if(prefs.contains("username"))
         {
             val username = prefs.getString("username", "")
-            setContentView(R.layout.activity_main)
+            InitMainView()
         }else {
             setContentView(R.layout.activity_firstlaunch)
             val usernameInput = findViewById<EditText>(R.id.usernameInput)
@@ -25,7 +68,7 @@ class MainActivity : AppCompatActivity() {
                 if (enteredName.isNotEmpty()) {
                     prefs.edit().putString("username", enteredName).apply()
                     StatusText.text = "Hello, $enteredName"
-                    setContentView(R.layout.activity_main)
+                    InitMainView()
                 } else {
                     StatusText.text = "Username cannot be empty"
                     StatusText.setTextColor(android.graphics.Color.parseColor("#FF8800"))
